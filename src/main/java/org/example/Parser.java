7e690becb4bd7df8d1f86f72535851142b63ecf2
alfonso gonzalez <alfonso@turo.com>
2023-03-14 19:32:12 +0000

    refactor: move get flag and remove double nested loops

--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -1,46 +1,51 @@
 package org.example;
 
-import java.util.Arrays;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Hello world!
- *
  */
-public class Parser
-{
+public class Parser {
     private Schema schema;
 
     public Parser(Schema schema) {
         this.schema = schema;
     }
 
-    // Throws runtimeexception when parser fails
+    // Throws runtime exception when parser fails
     public Map<String, Object> parse(String arg) {
         String[] tokens = arg.split(" ");
         Map<String, Object> results = new HashMap<>();
         for (int i = 0; i < tokens.length; i++) {
-            for (Flag flag : this.schema.flags) {
-                if (tokens[i].equals("-" + flag.flagName)) {
-                    if (flag.flagType == Boolean.class) {
-                        if (i + 1 < tokens.length) {
-                            if (tokens[i + 1].equals("true") ||
-                                    tokens[i + 1].startsWith("-")) {
-                                results.put(flag.flagName, true);
-                            } else if (tokens[i + 1].equals("false")) {
-                                results.put(flag.flagName, false);
-                            } else {
-                                throw new RuntimeException("Invalid arguments");
-                            }
-                        } else {
-                            results.put(flag.flagName, true);
-                        }
-                    }
-                }
+            Flag maybeFlag = this.schema.getFlag(tokens[i]);
+            if (maybeFlag != null) {
+                Map<String, Object> parsedFlag = parseFlag(tokens, i, maybeFlag);
+                results.putAll(parsedFlag);
             }
         }
         return results;
+    }
+
+    private static Map<String, Object> parseFlag(String[] tokens, int i, Flag maybeFlag) {
+        if (maybeFlag.flagType == Boolean.class) {
+            if (i + 1 < tokens.length) {
+                if (tokens[i + 1].equals("true") ||
+                        tokens[i + 1].startsWith("-")) {
+                    return Map.of(maybeFlag.flagName, true);
+                } else if (tokens[i + 1].equals("false")) {
+                    return Map.of(maybeFlag.flagName, false);
+                } else {
+                    throw new RuntimeException("Invalid arguments");
+                }
+            } else {
+                return Map.of(maybeFlag.flagName, true);
+            }
+        } else {
+            throw new RuntimeException("Not implemented");
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/org/example/Schema.java
+++ src/main/java/org/example/Schema.java
@@ -1,11 +1,22 @@
 package org.example;
 
 import java.util.Collection;
+import java.util.Objects;
 
 public class Schema {
     public Collection<Flag> flags;
 
     public Schema(Collection<Flag> flags) {
         this.flags = flags;
+    }
+
+
+    public Flag getFlag(final String flagName) {
+        for (Flag flag : flags) {
+            if (Objects.equals("-" + flag.flagName, flagName)) {
+                return flag;
+            }
+        }
+        return null;
     }
 }
